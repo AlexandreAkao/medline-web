52bd493411f22c9d7e070133ad076022be2d382c
Alexandre Akira <alexandreakiraakao@gmail.com>
2022-04-18 15:45:59 -0300

    chore(4): create Select/dropdown component

---
+++ src/components/Dropdown/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import { StylesConfig } from 'react-select';
+
+import { ButtonContainer } from 'components/Button/styles';
+
+export const DropdownContainer = styled.div`
+  display: flex;
+  border-radius: 12px;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  background-color: #fff;
+  align-items: center;
+
+  ${ButtonContainer} {
+    box-shadow: none;
+    margin-left: 40px;
+  }
+`;
+
+export const customStyleIcon = {
+  marginRight: '8px',
+};
+
+export const customStyleSelect: StylesConfig = {
+  menu: provided => ({
+    ...provided,
+  }),
+  container: provided => ({
+    ...provided,
+    flex: 1,
+  }),
+  control: provided => ({
+    ...provided,
+    border: 0,
+  }),
+  indicatorSeparator: provided => ({
+    ...provided,
+    width: 0,
+  }),
+};
