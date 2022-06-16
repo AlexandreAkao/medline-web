5abe39e617610f123e81788dc8bc82108a32aade
Alexandre Akira <alexandreakiraakao@gmail.com>
2022-06-16 12:33:17 -0300

    feat(38): create employee patient care page

---
+++ src/pages/EmployeePatienteCare/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+import { TableContainer } from 'components/Table/styles';
+import colors from 'styles/colors';
+
+export const EmployeePatienteCareContainer = styled.div`
+  background-color: ${colors.white.lighter};
+  height: 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position-x: 50%;
+  overflow: auto;
+`;
+
+export const EmployeePatienteCareMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 2rem auto;
+  width: 80%;
+
+  ${TableContainer} {
+    width: 100%;
+  }
+`;
+
+export const EmployeePatienteCareTitle = styled.h1`
+  font-size: 2.125rem;
+  font-weight: 500;
+  margin-bottom: 2rem;
+`;
